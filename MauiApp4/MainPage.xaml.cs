7cb5329c2164a6e7938189449d27d6d38af8f160
Pedro Jesus <pedrojesus.cefet@gmail.com>
2025-03-10 23:05:42 -0300

    init

---
+++ MauiApp4/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Maui.Controls.Shapes;
+
+namespace MauiApp4;
+
+public partial class MainPage
+{
+    CancellationTokenSource animateTimerCancellationTokenSource = new();
+    const uint CoverAnimationDuration = 5000;
+
+    public MainPage()
+    {
+        InitializeComponent();
+        
+        progress.SizeChanged += HandleProgressSizeChanged;
+    }
+    
+    void HandleProgressSizeChanged(object? sender, EventArgs e)
+    {
+        var minSize = Math.Min(progress.Height, progress.Width) - 50;
+
+        imgBorder.HeightRequest = imgBorder.WidthRequest = minSize;
+
+        var roundedRec = (RoundRectangle)imgBorder.StrokeShape!;
+
+        roundedRec.CornerRadius = new(minSize / 2);
+    }
+
+    bool isPlaying;
+    private void Button_OnClicked(object? sender, EventArgs e)
+    {
+        isPlaying = !isPlaying;
+        RotateCover(isPlaying);
+    }
+    
+    async void RotateCover(bool isPlaying)
+    {
+        if (isPlaying)
+        {
+            await StartCoverAnimation(animateTimerCancellationTokenSource.Token);
+        }
+        else
+        {
+            animateTimerCancellationTokenSource.Cancel();
+            animateTimerCancellationTokenSource.Dispose();
+            animateTimerCancellationTokenSource = new();
+        }
+    }
+
+    async Task StartCoverAnimation(CancellationToken tokenSource)
+    {
+        try
+        {
+            while (!tokenSource.IsCancellationRequested)
+            {
+                await albumArt.RelRotateTo(360, CoverAnimationDuration, Easing.Linear);
+            }
+
+            albumArt.CancelAnimations();
+
+        }
+        catch (TaskCanceledException ex)
+        {
+            albumArt.CancelAnimations();
+        }
+    }
+
+}
